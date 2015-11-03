b4f269cd0facdfc850b0354d7b51e683e23aac9b
micafer <micafer1@upv.es>
2015-11-03 14:25:16 +0100

    Minor bugfix

--- toscaparser/elements/nodetype.py
+++ toscaparser/elements/nodetype.py
@@ -155,9 +155,11 @@ class NodeType(StatefulEntityType):
         if parent_node:
             while parent_node.type != 'tosca.nodes.Root':
                 req = parent_node.get_value(self.REQUIREMENTS, None, True)
-                for r in req:
-                    if r not in requires:
-                        requires.append(r)
+                # This req can be None, add this if
+                if req:
+                    for r in req:
+                        if r not in requires:
+                            requires.append(r)
                 parent_node = parent_node.parent_type
         return requires
 
