17f19a3b0b110f12bbdb694f828a7ca2e2f738ed
micafer <micafer1@upv.es>
2016-06-10 10:11:59 +0200

    Add Token class to manage token function

--- toscaparser/functions.py
+++ toscaparser/functions.py
@@ -29,6 +29,7 @@ GET_PROPERTY = 'get_property'
 GET_ATTRIBUTE = 'get_attribute'
 GET_INPUT = 'get_input'
 CONCAT = 'concat'
+TOKEN = 'token'
 
 SELF = 'SELF'
 HOST = 'HOST'
@@ -636,11 +637,45 @@ class Concat(Function):
     def result(self):
         return self
 
+class Token(Function):
+    """Validate the function and provide an instance of the function
+
+    The token function is used within a TOSCA service template on a string to
+    parse out (tokenize) substrings separated by one or more token characters
+    within a larger string.
+
+
+    Arguments:
+
+    * The composite string that contains one or more substrings separated by
+      token characters.
+    * The string that contains one or more token characters that separate
+      substrings within the composite string.
+    * The integer indicates the index of the substring to return from the
+      composite string.  Note that the first substring is denoted by using
+      the '0' (zero) integer value.
+
+    Example:
+
+     [ get_attribute: [ my_server, data_endpoint, ip_address ], ':', 1 ]
+
+    """
+
+    def validate(self):
+        if len(self.args) < 3:
+            ExceptionCollector.appendException(
+                ValueError(_('Invalid arguments for function "{0}". Expected '
+                             'at least three arguments.').format(TOKEN)))
+
+    def result(self):
+        return self
+
 function_mappings = {
     GET_PROPERTY: GetProperty,
     GET_INPUT: GetInput,
     GET_ATTRIBUTE: GetAttribute,
-    CONCAT: Concat
+    CONCAT: Concat,
+    TOKEN: Token,
 }
 
 
