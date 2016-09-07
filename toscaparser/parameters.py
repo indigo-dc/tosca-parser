f6d8485e22c4ee6f4c0b07418e9d49c468caa2ac
micafer <micafer1@upv.es>
2016-09-07 11:45:44 +0200

    Add entry_schema to the list of properties fields

--- toscaparser/parameters.py
+++ toscaparser/parameters.py
@@ -28,8 +28,8 @@ log = logging.getLogger('tosca')
 class Input(object):
 
     INPUTFIELD = (TYPE, DESCRIPTION, DEFAULT, CONSTRAINTS, REQUIRED,
-                  STATUS) = ('type', 'description', 'default',
-                             'constraints', 'required', 'status')
+                  STATUS, ENTRY_SCHEMA) = ('type', 'description', 'default',
+                             'constraints', 'required', 'status', 'entry_schema')
 
     def __init__(self, name, schema_dict):
         self.name = name
