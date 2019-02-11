dda2f7d6e8b2bf1a9ed18753d8b3f9cc642b6e39
micafer <micafer1@upv.es>
2019-02-11 17:49:07 +0100

    Fix package generator

--- packages/generate_deb.sh
+++ packages/generate_deb.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 apt update
-apt install -y python-stdeb
+apt install -y python-stdeb python-pip
+pip install pytz pbr
 python setup.py --command-packages=stdeb.command sdist_dsc --depends "python-pbr, python-dateutil" bdist_deb
 mkdir dist_pkg
 cp deb_dist/*.deb dist_pkg
